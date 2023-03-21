172b681dce2602757836969fdddb12f97d6e1684
Andrei Christian Netoiu <andreichristiannetoiu@gmail.com>
2023-03-21 22:27:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const niceSticker = document.querySelector('.nice-sticker');
+
+// timer function that shows the sticker after 5 seconds
+
+setTimeout(() => {
+    niceSticker.classList.add('show');
+    }, 7000);
+
